4daff21b774f7c98f01508747d933a7868c3ace1
Joseph Shapiro <jshapiro33@gatech.edu>
2023-04-23 13:43:57 -0500

    Code organization in get_gpt_response

--- fun_with_punchers/get_gpt_response.py
+++ fun_with_punchers/get_gpt_response.py
@@ -18,13 +18,6 @@ def generate_text(prompt):
         presence_penalty=0
     )
 
-    print()
-    print()
-    print(response)
-    print()
-    print()
-    print()
-    print(vars(response))
     return response.choices[0].text.strip()
 
 
@@ -37,33 +30,28 @@ def generate_image(prompt):
         size="256x256"
     )
 
-    print()
-    print()
-    print(response)
-    print()
-    print()
-    print()
-    print(vars(response))
-    image_url = response['data'][0]['url']
+    return response['data'][0]['url']
 
-fighter_one_token_id = 4915
-fighter_one_info = testing_get_nft_info.get_puncher_info(fighter_one_token_id, 'Ninja')
-print(fighter_one_info)
 
-fighter_two_token_id = 3825
-fighter_two_info = testing_get_nft_info.get_puncher_info(fighter_two_token_id, 'TNG')
-print(fighter_two_info)
+def create_story_and_image(token_id_one, name_one, token_id_two, name_two):
+    fighter_one_info = testing_get_nft_info.get_puncher_info(token_id_one, name_one)
+    print(f"Gladiator 1 info: {fighter_one_info}")
 
-text_prompt = f"Describe an epic battle between two gladiators. Only one victor should remain standing at the end and it should be random which gladiator that is. Here is a description of gladiator 1: {fighter_one_info}. Here is a description of gladiator 2: {fighter_two_info}."
-print(text_prompt)
-story = generate_text(text_prompt)
-print()
-print()
-print()
-print()
-print()
-print(story)
+    fighter_two_info = testing_get_nft_info.get_puncher_info(token_id_two, name_two)
+    print(f"Gladiator 2 info: {fighter_two_info}")
 
-img_prompt = f"A hyper-realistic detailed painting of an epic battle between two gladiators in medieval arena. Gladiator one named {fighter_one_info}. Gladiator two named {fighter_two_info}."
-image = generate_image(img_prompt)
-print(image)
+    text_prompt = f"Describe an epic battle between two gladiators. Only one victor should remain standing at the end and it should be random which gladiator that is. Here is a description of gladiator 1: {fighter_one_info}. Here is a description of gladiator 2: {fighter_two_info}."
+    story = generate_text(text_prompt)
+    print()
+    print()
+    print()
+    print(story)
+
+    img_prompt = f"A hyper-realistic detailed painting of an epic battle between two gladiators in medieval arena. Gladiator one named {fighter_one_info}. Gladiator two named {fighter_two_info}."
+    image = generate_image(img_prompt)
+    print()
+    print()
+    print()
+    print(image)
+
+create_story_and_image(4915, 'Ninja', 3825, 'TNG')
